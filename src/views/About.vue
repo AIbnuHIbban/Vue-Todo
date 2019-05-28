4c1f824eb8919348121f3f419c944c95146cda64
Abdullah Ibnu Hibban <aibnuhibban@localhost.localdomain>
2019-05-28 21:37:06 +0700

    Fixed Bugs

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <center>
+    <div class="about">
+      <h1>Learning Vue2 - Success</h1>
+      <h3>Alhamdulillah Belajar Vue2 Buat Todo List pakai Array dan API "Fake"</h3>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero, voluptates sunt odio qui reiciendis incidunt. Possimus tenetur deserunt labore id quasi quidem tempore, distinctio commodi. Eos, mollitia. Reprehenderit, consectetur eos.</p>
+    </div>
+  </center>
 </template>
